3d146801a604e91aa33f4cc78e4c3b31132e8600
naresh7km <naresh7km@gmail.com>
2026-05-05 10:18:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const ORIGIN_GROUPS = {
     "https://relaxed-pegasus-8e3b77.netlify.app": { method: "s3ap", target: "https://pop-files-5.onrender.com" },
   },
   dmc: {
-    "https://miyabikinjp.d2uh9223qaxhlu.amplifyapp.com": { method: "iframe", target: "https://myfirstonforgesite-fhswmnvy.on-forge.com" },
+    "https://miyabikinjp.d2uh9223qaxhlu.amplifyapp.com": { method: "iframe", target: "https://dmctestnopublic.d2cgvxzk5gotzg.amplifyapp.com" },
   },
   aomine: {
     "https://zen-hawellness.life": { method: "iframe", target: "https://pop-files-5.onrender.com/aomine.html" },
