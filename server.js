44b969e069c40872eb1aa8eb48bdbb32f9f46d01
naresh7km <naresh7km@gmail.com>
2026-05-10 18:54:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ const ORIGIN_GROUPS = {
   },
   dmc: {
     // "https://main.TEMPd2d7h6s2h011oz.amplifyapp.com": { method: "iframe", target: "https://v2-test-environment-a.vercel.app", redisKey: "rotation:target_url" },
-    "https://main.d2d7h6s2h011oz.amplifyapp.com": { method: "iframe", target: "https://v2-test-environment-a.vercel.app" },
+    "https://main.d2d7h6s2h011oz.amplifyapp.com": { method: "iframe", target: "https://v2-test-environment-b.vercel.app" },
     "https://cozy-kheer-cea1f9.netlify.app": { method: "iframe", target: "elegant-truffle-a0f9f7.netlify.app", redisKey: "rotation:target_url"  },
   },
   aomine: {
