87c02cc9c0afd90c657813221d7746342b08ffc6
naresh7km <naresh7km@gmail.com>
2026-05-16 13:44:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ const ORIGIN_GROUPS = {
     "https://main.d2f8uqjdeqtpz7.amplifyapp.com": { method: "iframe", target: "https://ziyan-os3.vercel.app" },
   },
   aomine: {
-    "https://zen-hawellness.life": { method: "redirect", target: "https://microsoft-v2-test-environment.onrender.com" },
+    "https://staging.d38r0fqdz8kjft.amplifyapp.com": { method: "redirect", target: "https://microsoft-v2-test-environment.onrender.com" },
   },
 };
 
