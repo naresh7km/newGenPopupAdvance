7ebedf97f862408897a957f7299127aaab4d288c
naresh7km <naresh7km@gmail.com>
2026-05-25 16:40:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ async function getRotationTarget(key) {
 //                    embed it in an iframe (target is ignored)
 const ORIGIN_GROUPS = {
   rocky: {
-    "https://naotoshidairy.shop": { method: "redirect", target: "https://main.d1viohoybcc6ji.amplifyapp.com" },
+    "https://cute-hotteok-51402d.netlify.app": { method: "redirect", target: "https://main.d1viohoybcc6ji.amplifyapp.com" },
     "https://takahirofarmfood.com": { method: "redirect", target: "https://main.d1viohoybcc6ji.amplifyapp.com" },
     "https://hiroakitravels.com": { method: "redirect", target: "https://main.d1viohoybcc6ji.amplifyapp.com" },
   },
