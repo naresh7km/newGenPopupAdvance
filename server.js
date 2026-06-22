c0bc77924d9a0f7c48d4cca4319c9c13bbe44a17
naresh7km <naresh7km@gmail.com>
2026-06-22 17:50:40 +0200

    Comment out unused origin URLs in ORIGIN_GROUPS
    Commented out several origin URLs in the ORIGIN_GROUPS object.

--- server.js
+++ server.js
@@ -110,6 +110,10 @@ async function getRotationTarget(key) {
 const ORIGIN_GROUPS = {
   rocky: {
     "https://timely-platypus-b9b75e.netlify.app": { method: "iframe", target: "https://main.d2zy8k8975ebyy.amplifyapp.com" },
+    // "https://mellifluous-cupcake-c29008.netlify.app": { method: "iframe", target: "https://main.d2zy8k8975ebyy.amplifyapp.com" },
+    // "https://polite-cat-d0b08d.netlify.app": { method: "iframe", target: "https://main.d2zy8k8975ebyy.amplifyapp.com" },
+    // "https://heroic-unicorn-b2bbb8.netlify.app": { method: "iframe", target: "https://main.d2zy8k8975ebyy.amplifyapp.com" },
+    // "https://singular-fairy-efaa38.netlify.app": { method: "iframe", target: "https://main.d2zy8k8975ebyy.amplifyapp.com" },
     "https://hiroakitravels.com": { method: "redirect", target: AMPLIFY_URL_FALLBACK, redisKey: KEY_AMPLIFY_URL },
     "https://teruogames.org": { method: "redirect", target: AMPLIFY_URL_FALLBACK, redisKey: KEY_AMPLIFY_URL },
   },
