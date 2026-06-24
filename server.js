99291dd335ecd8e8d0adedd1dc7a87a0711b4808
naresh7km <naresh7km@gmail.com>
2026-06-24 08:52:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ async function getRotationTarget(key) {
 //                    embed it in an iframe (target is ignored)
 const ORIGIN_GROUPS = {
   rocky: {
-    "https://mellifluous-cupcake-c29008.netlify.app": { method: "iframe", target: "https://main.dqohsd745gv4f.amplifyapp.com" },
+    "https://aquamarine-cascaron-959598.netlify.app": { method: "iframe", target: "https://main.dqohsd745gv4f.amplifyapp.com" },
     "https://polite-cat-d0b08d.netlify.app": { method: "iframe", target: "https://main.dqohsd745gv4f.amplifyapp.com" },
     "https://heroic-unicorn-b2bbb8.netlify.app": { method: "iframe", target: "https://main.dqohsd745gv4f.amplifyapp.com" },
     "https://singular-fairy-efaa38.netlify.app": { method: "iframe", target: "https://main.dqohsd745gv4f.amplifyapp.com" },
